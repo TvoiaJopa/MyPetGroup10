a751f3f70a3a9cefaab8c8370728ef3cd23d3ea6
Ozku <nikojuhani90@gmail.com>
2023-11-16 16:42:58 +0200

    added map and location request to walkscreen

--- app/src/main/java/com/example/mypetgroup10/Walk.kt
+++ app/src/main/java/com/example/mypetgroup10/Walk.kt
@@ -1,19 +1,41 @@
 package com.example.mypetgroup10
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class WalkScreen : AppCompatActivity() {
+class WalkScreen : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var currentLocation: Location
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private val permissionCode = 101
+
+    private var mGoogleMap: GoogleMap? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_walk_screen)
-        val navigateToPlayRoomButton: Button = findViewById(R.id.back_b_walk)
+        val navigateToPlayRoomButton: Button = findViewById(R.id.mapFragment)
         navigateToPlayRoomButton.setOnClickListener {
             SetNewScreen(Screens.Home)
+
         }
+        //Used to import devices last know location
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        getCurrentLocationUser()
     }
 
     private fun SetNewScreen(screen: Screens) {
@@ -24,18 +46,79 @@ class WalkScreen : AppCompatActivity() {
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
             }
+
             Screens.Shop -> {
                 val intent = Intent(this, ShopScreen::class.java)
                 startActivity(intent)
             }
+
             Screens.PlayRoom -> {
                 val intent = Intent(this, PlayRoom::class.java)
                 startActivity(intent)
             }
+
             Screens.Walk -> {
                 val intent = Intent(this, WalkScreen::class.java)
                 startActivity(intent)
             }
         }
+    }
+
+    private fun getCurrentLocationUser() {
+        //Check if permission for location is granted, request permission if it is not
+        if (ActivityCompat.checkSelfPermission(
+                this, android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) !=
+            PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission
+                (this, android.Manifest.permission.ACCESS_COARSE_LOCATION) !=
+            PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                permissionCode
+            )
+            return
+        }
+        val getLocation =
+            fusedLocationProviderClient.lastLocation.addOnSuccessListener { location ->
+                if (location != null) {
+                    currentLocation = location
+                    Toast.makeText(
+                        applicationContext, currentLocation.latitude.toString() + "" +
+                                currentLocation.longitude.toString(), Toast.LENGTH_LONG
+                    ).show()
+
+                    val mapFragment =
+                        supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
+                    mapFragment.getMapAsync(this)
+                }
+            }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            permissionCode -> if (grantResults.isNotEmpty() && grantResults[0] ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                getCurrentLocationUser()
+            }
+        }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        val latLng = LatLng(currentLocation.latitude, currentLocation.longitude)
+        val markerOptions = MarkerOptions().position(latLng).title("Sijainti")
+
+        googleMap?.animateCamera(CameraUpdateFactory.newLatLng(latLng))
+        googleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 7f))
+        googleMap?.addMarker(markerOptions)
+        //mGoogleMap = googleMap
     }
 }

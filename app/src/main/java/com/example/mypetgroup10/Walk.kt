ef6d4be33d8c1626c3c1f5c74bd82e739914e1b8
Ozku <nikojuhani90@gmail.com>
2023-11-16 18:10:12 +0200

    fixed crash on entering walkscreen

--- app/src/main/java/com/example/mypetgroup10/Walk.kt
+++ app/src/main/java/com/example/mypetgroup10/Walk.kt
@@ -27,7 +27,7 @@ class WalkScreen : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_walk_screen)
-        val navigateToPlayRoomButton: Button = findViewById(R.id.mapFragment)
+        val navigateToPlayRoomButton: Button = findViewById(R.id.back_b_walk)
         navigateToPlayRoomButton.setOnClickListener {
             SetNewScreen(Screens.Home)
 
